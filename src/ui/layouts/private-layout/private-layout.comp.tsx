8a7efefba3c1256ab7252f1c06e3fceeeaf88b67
Jean Saints <57118447+JSaints-Dev@users.noreply.github.com>
2024-11-14 12:57:15 -0300

    feat: added logout and redirect to home

--- src/ui/layouts/private-layout/private-layout.comp.tsx
+++ src/ui/layouts/private-layout/private-layout.comp.tsx
@@ -1,20 +1,34 @@
 import { Button } from '../../shared';
 import './private-layout.styles.css'
 import logoFullImage from '../../../assets/logo-full.svg';
+import { removeToken } from '../../../services';
+import { useNavigate } from 'react-router-dom';
+import { routes } from '../../../router';
 
 export type PrivateLayoutProps = {
   children: React.ReactNode;
 }
 
 export function PrivateLayout({children}: PrivateLayoutProps) {
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    removeToken();
+    navigate(routes.LOGIN);
+  }
+
+  function handleNavigateToHome() {
+    navigate(routes.HOME);
+  }
+
   return (
     <main className="private__layout__container">
       <div className="menu__container">
         <div className="menu__container__content">
-          <div className="menu__container__logo">
+          <div className="menu__container__logo" onClick={handleNavigateToHome}>
             <img src={logoFullImage} alt="Cora" title="Cora" />
           </div>
-          <Button variant='outline__white'>Sair</Button>
+          <Button onClick={handleLogout} variant='outline__white'>Sair</Button>
         </div>
       </div>
       {children}
